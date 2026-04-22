2083156b0aaf26e5f25cfb20d61f5c4a11cbea01
Amet Alvirde <Amet.Alvirde@gmail.com>
2026-04-22 17:30:18 -0400

    feat: Adds syntax highlighting for css`` expressions
    via styled on nvim-treesitter

--- version.lua
+++ version.lua
@@ -1,6 +1,6 @@
   return {
-  version = "1.2.0",
-  build_date = "2026-04-07",
+  version = "1.4.0",
+  build_date = "2026-04-22",
   neovim_min_version = "0.11.0",
   description = "AmetAlvirde's personal dotNvim from scratch configuration",
   obsidian_version = "1.12.7",
